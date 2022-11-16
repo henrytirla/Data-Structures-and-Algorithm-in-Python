13cab6c34ecf13aacdd8481c807f2968aabeba02
Henry Tirla <henrytirla@gmail.com>
2022-11-16 22:40:40 +0000

    R-1.2

--- Chapter 1/Reinforcement/R-1.2.py
+++ Chapter 1/Reinforcement/R-1.2.py
@@ -1,3 +1,16 @@
 """Write a short Python function, is even(k), that takes an integer value and
 returns True if k is even, and False otherwise. However, your function
 cannot use the multiplication, modulo, or division operators."""
+
+def is_even(k):
+    n = 2
+    while k>n:
+       n+=2
+    if n-k == 1:
+       return False
+
+    return True
+
+
+print(is_even(17))
+
