c69b961e1e5d8b5b9bd450ac66c6b2cf6d480911
Henry Tirla <henrytirla@gmail.com>
2022-11-18 05:23:30 +0000

    solution

--- Chapter 1/Reinforcement/R-1.5.py
+++ Chapter 1/Reinforcement/R-1.5.py
@@ -1,2 +1,8 @@
 """Give a single command that computes the sum from Exercise R-1.4, relying
 on Python’s comprehension syntax and the built-in sum function."""
+
+def sum_of_squares2(n):
+    return sum([k * k for k in range(0, n)])
+
+
+print(sum_of_squares2(4))
