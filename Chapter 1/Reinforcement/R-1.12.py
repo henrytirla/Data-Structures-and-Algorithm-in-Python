90ef27079d2653f1db093ad2808b83eea046b2b6
Henry Tirla <henrytirla@gmail.com>
2022-11-18 13:38:04 +0000

    solution

--- Chapter 1/Reinforcement/R-1.12.py
+++ Chapter 1/Reinforcement/R-1.12.py
@@ -4,3 +4,13 @@ a more basic function randrange, with parameterization similar to
 the built-in range function, that return a random choice from the given
 range. Using only the randrange function, implement your own version
 of the choice function."""
+
+from random import randrange
+
+def my_choice(data):
+    x = randrange(0,len(data))
+    print(x)
+    return data[x]
+
+
+print(my_choice([1,2,3,4,5,6,7]))
--- Chapter 1/Reinforcement/R-1.8.py
+++ Chapter 1/Reinforcement/R-1.8.py
@@ -2,3 +2,13 @@
 such as a string. If string s has length n, and expression s[k] is used for index
 −n≤k<0, what is the equivalent index j ≥0 such that s[j] references
 the same element?"""
+
+s = "Henry"
+n = len(s)
+
+for k in range(-n, 0):
+    print(s[k],k)
+
+
+for j in range(n):
+    print(s[j],j)
