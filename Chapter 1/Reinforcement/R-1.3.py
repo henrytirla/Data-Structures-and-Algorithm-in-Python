a6355e91efe6af3765b54da4e554eaf9677bb2ba
Henry Tirla <henrytirla@gmail.com>
2022-11-18 04:59:19 +0000

    R-1.3

--- Chapter 1/Reinforcement/R-1.3.py
+++ Chapter 1/Reinforcement/R-1.3.py
@@ -3,3 +3,23 @@ one or more numbers, and returns the smallest and largest numbers, in the
 form of a tuple of length two. Do not use the built-in functions min or
 max in implementing your solution.
 """
+
+def minmax(data):
+    largest_num =data[0]
+    smallest_num =data[0]
+    for num in data:
+        if num > largest_num:
+            largest_num =num
+        elif num < smallest_num:
+            smallest_num = num
+    return largest_num,smallest_num
+
+
+print(minmax([6,1,2,5,9,12]))
+
+
+
+
+
+
+#print(minmax([1,2,3,6]))
