f1a70780384a6198b5a5d768efa7d3fdd4011c46
Henry Tirla <henrytirla@gmail.com>
2022-11-18 05:42:31 +0000

    solution

--- Chapter 1/Reinforcement/R-1.7.py
+++ Chapter 1/Reinforcement/R-1.7.py
@@ -1,2 +1,8 @@
 """Give a single command that computes the sum from Exercise R-1.6, relying
 on Python’s comprehension syntax and the built-in sum function."""
+
+def sum_of_squares_odd2(n):
+    return sum([k * k for k in range(n) if k % 2 != 0])
+
+
+print(sum_of_squares_odd2(4))
