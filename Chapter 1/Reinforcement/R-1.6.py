5489ef3da69f6d88a730cc546e7a8e3dac171189
Henry Tirla <henrytirla@gmail.com>
2022-11-18 05:39:54 +0000

    solution

--- Chapter 1/Reinforcement/R-1.6.py
+++ Chapter 1/Reinforcement/R-1.6.py
@@ -1,2 +1,13 @@
 """Write a short Python function that takes a positive integer n and returns
 the sum of the squares of all the odd positive integers smaller than n."""
+
+def odd_squares(n):
+    sum_n=0
+
+    for i in range(n):
+        if i % 2 != 0:
+            sum_n+= i*i
+
+    return sum_n
+
+print(odd_squares(7))
