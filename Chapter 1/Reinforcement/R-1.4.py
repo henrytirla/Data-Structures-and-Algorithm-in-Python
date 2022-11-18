27c17e5f55d2cf55e9e7938f565de12d0e57cec9
Henry Tirla <henrytirla@gmail.com>
2022-11-18 05:20:13 +0000

    R-1.4

--- Chapter 1/Reinforcement/R-1.4.py
+++ Chapter 1/Reinforcement/R-1.4.py
@@ -1,2 +1,11 @@
 """Write a short Python function that takes a positive integer n and returns
 the sum of the squares of all the positive integers smaller than n."""
+
+def Sum_squares(n):
+    sum_n = 0
+    while n >0:
+        n -=1
+        sum_n += n*n
+    return  sum_n
+
+print(Sum_squares(4))
