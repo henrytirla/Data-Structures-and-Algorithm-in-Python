e0a6390de05328bf915354766b341a4758b2527b
Henry Tirla <henrytirla@gmail.com>
2022-11-22 05:55:26 +0000

    Questions

--- Chapter 1/Projects/P-1.29.py
+++ Chapter 1/Projects/P-1.29.py
@@ -1,10 +1,2 @@
-"""Write a short Python function, is multiple(n, m), that takes two integer
-values and returns True if n is a multiple of m, that is, n = mi for some
-integer i, and False otherwise."""
-
-def multiple(n,m):
-    if n%m == 0:
-        return True
-    return False
-
-print(multiple(12,6))
+"""Write a Python program that outputs all possible strings formed by using
+the characters c , a , t , d , o , and g exactly once."""
