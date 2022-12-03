73c960cef68f7d87ade03c53af1126847bba68c3
Henry Tirla <henrytirla@gmail.com>
2022-12-03 02:26:01 +0000

    solution

--- Chapter 2/Reinforcement/R-2.1.py
+++ Chapter 2/Reinforcement/R-2.1.py
@@ -7,7 +7,7 @@
 
 Life critical software system or application is a system 
 whose malfunction or damage may cause serious issues 
-to the people involved. The outcome may be as follows:
+to the people involved.
 
 Automatic braking, 
 cruise control, 
