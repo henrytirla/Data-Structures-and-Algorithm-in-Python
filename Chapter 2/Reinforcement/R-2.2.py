9b306f12671199d202fc3782fe646b35d3cdc385
Henry Tirla <henrytirla@gmail.com>
2022-12-03 02:50:57 +0000

    solution

--- Chapter 2/Reinforcement/R-2.2.py
+++ Chapter 2/Reinforcement/R-2.2.py
@@ -7,17 +7,10 @@ the difference between a prolonged lifetime of sales and bankruptcy."""
 
 eg Microsoft word , Facebook
 
-Let's take Facebook as an example, when it was started it's was just a small application providing 
-service around the university campus, maintaining one server. Now it's a major platform 
-providing service for the billions of people around the globe.
-the software adapts over time with new requirements to meet the needs of people
-
- 
-
-Take any major software application, it adapts over time with new needs. 
-They wouldn't have sustained in the market if not for the adaptability.
- 
-
-With the ever-growing data and rapid technology growth, 
-the inability to adopt new requirements means bankruptcy.
+One example of a software application in which adaptability can mean the difference between success and failure is a social media platform. 
+In the constantly changing world of social media, new technologies and trends emerge regularly. 
+If a social media platform is not able to adapt to these changes and offer new and innovative features to its users, 
+it risks losing users to competitors who are able to offer a better user experience. 
+This can ultimately lead to a decline in sales and even bankruptcy.
+ On the other hand, a social media platform that is able to adapt to changes and continuously improve its user experience can maintain a loyal user base and sustain long-term success.
 """
--- Chapter 2/Reinforcement/R-2.4.py
+++ Chapter 2/Reinforcement/R-2.4.py
@@ -4,3 +4,29 @@ of petals, and its price. Your class must include a constructor method
 that initializes each variable to an appropriate value, and your class should
 include methods for setting the value of each type, and retrieving the value
 of each type."""
+
+
+
+class Flower:
+    def __init__(self, name: str, num_petals: int, price: float):
+        self.name = name
+        self.num_petals = num_petals
+        self.price = price
+
+    def set_name(self, name: str) -> None:
+        self.name = name
+
+    def set_num_petals(self, num_petals: int) -> None:
+        self.num_petals = num_petals
+
+    def set_price(self, price: float) -> None:
+        self.price = price
+
+    def get_name(self) -> str:
+        return self.name
+
+    def get_num_petals(self) -> int:
+        return self.num_petals
+
+    def get_price(self) -> float:
+        return self.price
