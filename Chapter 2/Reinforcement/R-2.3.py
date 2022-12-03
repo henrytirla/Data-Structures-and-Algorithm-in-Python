4c5a1a064835d6d0a3e5aaa72de2c3b4f851e15d
Henry Tirla <henrytirla@gmail.com>
2022-12-03 02:55:11 +0000

    solution

--- Chapter 2/Reinforcement/R-2.3.py
+++ Chapter 2/Reinforcement/R-2.3.py
@@ -1,4 +1,18 @@
 """
 
 Describe a component from a text-editor GUI and the methods that it encapsulates.
+
+
+
+
+
+
+One component from a text-editor GUI is the text editor itself, which allows the user to input and edit text.
+This component would likely encapsulate methods for inserting and deleting text, selecting text, copying and pasting text,
+ undoing and redoing actions, and formatting text (e.g. changing font size and color). It might also include methods for
+ saving and loading files, and for searching and replacing text within the editor. These methods would provide a simple
+  and intuitive interface for the user to edit and manipulate text within the text editor
 """
+
+
+
