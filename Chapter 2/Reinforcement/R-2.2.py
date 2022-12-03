7cd631d48d98791d992169b6a6f25c9e85afab8e
Henry Tirla <henrytirla@gmail.com>
2022-12-03 02:26:36 +0000

    solution

--- Chapter 2/Reinforcement/R-2.2.py
+++ Chapter 2/Reinforcement/R-2.2.py
@@ -1,2 +1,23 @@
 """Give an example of a software application in which adaptability can mean
 the difference between a prolonged lifetime of sales and bankruptcy."""
+
+
+
+"""
+
+eg Microsoft word , Facebook
+
+Let's take Facebook as an example, when it was started it's was just a small application providing 
+service around the university campus, maintaining one server. Now it's a major platform 
+providing service for the billions of people around the globe.
+the software adapts over time with new requirements to meet the needs of people
+
+ 
+
+Take any major software application, it adapts over time with new needs. 
+They wouldn't have sustained in the market if not for the adaptability.
+ 
+
+With the ever-growing data and rapid technology growth, 
+the inability to adopt new requirements means bankruptcy.
+"""
