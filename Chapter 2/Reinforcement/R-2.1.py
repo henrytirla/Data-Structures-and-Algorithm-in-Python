27bdac76903a006e081cffe5a9994dafe63da676
Henry Tirla <henrytirla@gmail.com>
2022-12-03 02:53:17 +0000

    solution

--- Chapter 2/Reinforcement/R-2.1.py
+++ Chapter 2/Reinforcement/R-2.1.py
@@ -9,9 +9,15 @@ Life critical software system or application is a system
 whose malfunction or damage may cause serious issues 
 to the people involved.
 
-Automatic braking, 
-cruise control, 
-lane control, computer 
-vision, obstacle recognition, 
-electronic engine control modules
+
+
+1. Medical systems: Medical systems are used in hospitals and other healthcare settings to manage patient records, 
+diagnose diseases, and monitor vital signs. These systems are critical for ensuring the health and 
+wellbeing of patients, and any failure or malfunction could have serious consequences.
+2. Aviation systems: Aviation systems are used in the aviation industry to control and monitor aircraft, 
+as well as manage flight schedules and operations. 
+These systems are critical for ensuring the safety of passengers and crew, and any failure or malfunction could lead to accidents.
+3. Nuclear power plant control systems: Nuclear power plant control systems are used to monitor and control the operation of nuclear reactors. 
+These systems are critical for ensuring the safe and efficient operation of nuclear power plants, and any failure or malfunction could have catastrophic consequences.
+
 """
