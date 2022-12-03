22375269e0a15262b0a2a7c916a6e8f2a6a76331
Henry Tirla <henrytirla@gmail.com>
2022-12-03 02:18:25 +0000

    solution

--- Chapter 2/Reinforcement/R-2.1.py
+++ Chapter 2/Reinforcement/R-2.1.py
@@ -1,1 +1,17 @@
 """"Give three examples of life-critical software applications."""
+
+
+
+"""
+
+
+Life critical software system or application is a system 
+whose malfunction or damage may cause serious issues 
+to the people involved. The outcome may be as follows:
+
+Automatic braking, 
+cruise control, 
+lane control, computer 
+vision, obstacle recognition, 
+electronic engine control modules
+"""
